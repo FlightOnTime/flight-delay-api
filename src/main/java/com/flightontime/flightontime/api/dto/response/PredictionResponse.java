2ab2d61f383dee6f3bf29976f1dfe2231f0c90aa
cristiellesreis <cristielle.santos94@gmail.com>
2025-12-15 06:52:36 -0300

    feat: cria dto de resposta

--- src/main/java/com/flightontime/flightontime/api/dto/response/PredictionResponse.java
+++ src/main/java/com/flightontime/flightontime/api/dto/response/PredictionResponse.java
@@ -1,4 +1,20 @@
 package com.flightontime.flightontime.api.dto.response;
 
 public class PredictionResponse {
+
+    private String status;
+    private double probabilidade;
+
+    public PredictionResponse(String status, double probabilidade) {
+        this.status = status;
+        this.probabilidade = probabilidade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public double getProbabilidade() {
+        return probabilidade;
+    }
 }

b2f1eb48ddc0819c4b38dfe1f66af32dcde6cdad
cristiellesreis <cristielle.santos94@gmail.com>
2025-12-30 07:26:45 -0300

    feat: implementa client para comunicacao com ml

--- src/main/java/com/flightontime/flightontime/config/SpringDocConfigurations.java
+++ src/main/java/com/flightontime/flightontime/config/SpringDocConfigurations.java
@@ -1,4 +1,4 @@
-package com.flightontime.flightontime.config.springdoc;
+package com.flightontime.flightontime.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
